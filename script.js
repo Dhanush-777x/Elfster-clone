ceb8c2e8bcea8d5c4bbe18d6701d9fbcf8f53123
dhanush <dhanushmani1504@gmail.com>
2024-03-17 13:19:16 +0530

    Added form validation and adding names functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+let inputCount = 4;
+
+function validateForm(event) {
+    const nameInput = document.getElementById('name');
+    const nameWarning = document.getElementById('nameWarning');
+    if (nameInput.value.trim() === '') {
+      nameWarning.style.display = 'block';
+      nameInput.style.borderColor = "#ff0000";
+      nameInput.focus();
+      event.preventDefault();
+    } else {
+      nameWarning.style.display = 'none';
+      nameInput.style.borderColor = "teal";
+    }
+  }
+
+
+
+function addNewNameInput() {
+  const otherNamesContainer = document.querySelector('.other-names-container');
+  const newInput = document.createElement('input');
+  newInput.type = 'text';
+  newInput.placeholder = `Enter Name ${inputCount + 1}`;
+  otherNamesContainer.appendChild(newInput);
+  inputCount++;
+}
