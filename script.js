451116718d54f693570b131a10bf55e3b3460e47
dhanush <dhanushmani1504@gmail.com>
2024-03-18 08:43:21 +0530

    curve bg fix

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 let inputCount = 4;
 
 function validateForm(event) {
-    const nameInput = document.getElementById('name');
-    const nameWarning = document.getElementById('nameWarning');
-    if (nameInput.value.trim() === '') {
-      nameWarning.style.display = 'block';
-      nameInput.style.borderColor = "#ff0000";
-      nameInput.focus();
-      event.preventDefault();
-    } else {
-      nameWarning.style.display = 'none';
-      nameInput.style.borderColor = "teal";
-    }
+  const nameInput = document.getElementById("name");
+  const nameWarning = document.getElementById("nameWarning");
+  if (nameInput.value.trim() === "") {
+    nameWarning.style.display = "block";
+    nameInput.style.borderColor = "#ff0000";
+    nameInput.focus();
+    event.preventDefault();
+  } else {
+    nameWarning.style.display = "none";
+    nameInput.style.borderColor = "teal";
   }
-
-
+}
 
 function addNewNameInput() {
-  const otherNamesContainer = document.querySelector('.other-names-container');
-  const newInput = document.createElement('input');
-  newInput.type = 'text';
+  const otherNamesContainer = document.querySelector(".other-names-container");
+  const newInput = document.createElement("input");
+  newInput.type = "text";
   newInput.placeholder = `Enter Name ${inputCount + 1}`;
   otherNamesContainer.appendChild(newInput);
   inputCount++;
